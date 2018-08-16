2345a1238b9adc884174593e0f6123785fef2cc1
LZR\admin <Lizhuoran1024@gmail.com>
2018-08-16 12:59:10 +0800

    commit 2018-0816 1258
    Slider control and play mode change complete.

--- PlayControl/PlayControlAction.py
+++ PlayControl/PlayControlAction.py
@@ -1,7 +1,7 @@
 from PlayControl import Ui_MainWindow
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import pyqtSlot, QTimer
+from PyQt5.QtCore import pyqtSlot, QTimer, QtDebugMsg
 from PyQt5.QtWidgets import QFileDialog, QSlider
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent, QMediaPlaylist
 from PyQt5.QtCore import QUrl
@@ -26,15 +26,34 @@ class PlayControlAction(QMainWindow, Ui_MainWindow):
 
         self.playPosSlider.setEnabled(False)
 
+        self.playModePanel.setVisible(False)
+        self.playModePanel.setEnabled(False)
+
         self.openFileButton.clicked.connect(self.openFile)
+
         self.playCtrlButton.clicked.connect(self.playAndPause)
+
         self.nextSongButton.clicked.connect(self.nextSong)
+
         self.prevSongButton.clicked.connect(self.prevSong)
-        # self.player.positionChanged.connect(self.showTime)
+
         self.player.durationChanged.connect(self.calcTotalTime)
-        # self.playPosSlider.sliderMoved.connect(self.dragSlider)
-        # self.playPosSlider.sliderReleased.connect(self.sliderRelease)
-        # self.timer.timeout.connect(self.showTime)
+
+        self.playPosSlider.sliderMoved.connect(self.dragSlider)
+
+        self.playPosSlider.sliderReleased.connect(self.sliderRelease)
+
+        self.timer.timeout.connect(self.showTime)
+
+        self.playModeChangeButton.clicked.connect(self.showPlayModePanel)
+
+        self.seqPlay.clicked.connect(self.setPlayMode)
+
+        self.itemOncePlay.clicked.connect(self.setPlayMode)
+
+        self.randomPlay.clicked.connect(self.setPlayMode)
+
+
 
     def openFile(self):
         self.filename_list = QFileDialog.getOpenFileNames(self, 'Open File', './',
@@ -44,14 +63,15 @@ class PlayControlAction(QMainWindow, Ui_MainWindow):
             self.play_list.clear()
             self.player.stop()
 
+            self.playPosSlider.setEnabled(False)
+            self.playPosSlider.setValue(0)
+
             for file in self.filename_list:
                 self.play_list.addMedia(QMediaContent(QUrl.fromLocalFile(file)))
 
             self.play_list.setCurrentIndex(0)
-            self.play_list.setPlaybackMode(QMediaPlaylist.CurrentItemInLoop)
+            self.play_list.setPlaybackMode(QMediaPlaylist.Random)
 
-            # self.playPosSlider.setValue(0)
-            # self.playPosSlider.setEnabled(False)
             self.loadPlayList()
             self.playSong()
 
@@ -62,6 +82,8 @@ class PlayControlAction(QMainWindow, Ui_MainWindow):
         current_song_name = self.filename_list[self.play_list.currentIndex()]
         current_song_name = current_song_name.split('/')[-1].split('.')[-2]
         self.songInfoLabel.setText(current_song_name)
+
+        self.playPosSlider.setEnabled(True)
 
         self.player.setMedia(self.play_list.currentMedia())
         self.player.play()
@@ -85,24 +107,28 @@ class PlayControlAction(QMainWindow, Ui_MainWindow):
             self.timer.start()
 
     def nextSong(self):
-        song_count = self.play_list.mediaCount()
-        current_song_index = self.play_list.currentIndex()
-        if current_song_index + 1 < song_count:
-            self.play_list.setCurrentIndex(current_song_index + 1)
-            self.playSong()
-        else:
-            self.play_list.setCurrentIndex(0)
-            self.playSong()
+        self.play_list.next()
+        self.playSong()
+        # song_count = self.play_list.mediaCount()
+        # current_song_index = self.play_list.currentIndex()
+        # if current_song_index + 1 < song_count:
+        #     self.play_list.setCurrentIndex(current_song_index + 1)
+        #     self.playSong()
+        # else:
+        #     self.play_list.setCurrentIndex(0)
+        #     self.playSong()
 
     def prevSong(self):
-        song_count = self.play_list.mediaCount()
-        current_song_index = self.play_list.currentIndex()
-        if current_song_index - 1 >= 0:
-            self.play_list.setCurrentIndex(current_song_index - 1)
-            self.playSong()
-        else:
-            self.play_list.setCurrentIndex(song_count - 1)
-            self.playSong()
+        self.play_list.previous()
+        self.playSong()
+        # song_count = self.play_list.mediaCount()
+        # current_song_index = self.play_list.currentIndex()
+        # if current_song_index - 1 >= 0:
+        #     self.play_list.setCurrentIndex(current_song_index - 1)
+        #     self.playSong()
+        # else:
+        #     self.play_list.setCurrentIndex(song_count - 1)
+        #     self.playSong()
 
     def showTime(self):
         if (self.player.mediaStatus() != self.player.NoMedia) and (self.song_len_time != '0:0'):
@@ -118,8 +144,6 @@ class PlayControlAction(QMainWindow, Ui_MainWindow):
             self.song_len_time = f'{(song_len//1000)//60}:{(song_len//1000)%60}'
             self.playPosSlider.setEnabled(True)
 
-            # self.playPosSlider.setTickInterval(1)
-            # self.playPosSlider.setTickPosition(0)
             self.playPosSlider.setRange(0, 1000)
 
     def changeSlider(self):
@@ -134,3 +158,26 @@ class PlayControlAction(QMainWindow, Ui_MainWindow):
         self.player.setPosition(self.playPosSlider.value() * self.player.duration() // 1000)
         self.player.play()
         self.timer.start()
+        self.is_playing = True
+        self.playCtrlButton.setText('Pause')
+
+    def showPlayModePanel(self):
+
+        self.playModePanel.setEnabled(True)
+        self.playModePanel.setVisible(True)
+
+    def setPlayMode(self):
+        sender = self.sender()
+
+        if sender.text() == 'Seq':
+            self.play_list.setPlaybackMode(self.play_list.Loop)
+            self.playModeChangeButton.setText('Seq')
+        elif sender.text() == 'Once':
+            self.play_list.setPlaybackMode(self.play_list.CurrentItemInLoop)
+            self.playModeChangeButton.setText('Once')
+        elif sender.text() == 'Random':
+            self.play_list.setPlaybackMode(self.play_list.Random)
+            self.playModeChangeButton.setText('Random')
+
+        self.playModePanel.setVisible(False)
+        self.playModePanel.setEnabled(False)
